cb2ba1a1e87c6ddbce7ba4c9cb92167ef4f2995d
ProWolf <prowolfmailbox@gmail.com>
2017-10-15 20:29:26 +0000

    Added user table seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
