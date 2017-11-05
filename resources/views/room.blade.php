16bfe0bcf485adea2bf1ba4e351e82091097f066
ProWolf <prowolfmailbox@gmail.com>
2017-11-05 17:16:48 +0000

    Added Font Awesome support

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -2,12 +2,16 @@
 
 @section('title', $room->name)
 
+@section('styles')
+    <link rel="stylesheet" href="https://use.fontawesome.com/7fb48413eb.css">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $room->name }}</div>
+                <div class="panel-heading">{{ $room->name }}<i class="fa fa-info-circle fa-2x pull-right align-middle" title="info" @click="info"></i></div>
 
                 <div class="panel-body">
                     @if(Auth::check())
